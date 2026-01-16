4b0e30226a858b1e49b305ed6ab937633092b13d
GuidoMagni <guibonmagni@gmail.com>
2026-01-16 19:20:39 +0100

    updated get_next_line

--- get_next_line.h
+++ get_next_line.h
@@ -3,16 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: guido <guido@student.42.fr>                +#+  +:+       +#+        */
+/*   By: gumagni <gumagni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 18:01:11 by gumagni           #+#    #+#             */
-/*   Updated: 2026/01/13 16:44:05 by guido            ###   ########.fr       */
+/*   Updated: 2026/01/16 18:25:07 by gumagni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 10
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -22,9 +24,11 @@
 # include <fcntl.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
+//char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(char *src);
+void	fill_str(char *res, char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,52 +6,74 @@
 /*   By: gumagni <gumagni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 18:01:08 by gumagni           #+#    #+#             */
-/*   Updated: 2026/01/10 19:38:51 by gumagni          ###   ########.fr       */
+/*   Updated: 2026/01/16 18:20:15 by gumagni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_substr(char *s, unsigned int start, size_t len)
 {
-	char	*final;
 	size_t	i;
-	size_t	len_tot;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (malloc(1));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char			*res;
+
+	res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	fill_str(res, s1, s2);
+	return (res);
+}
+
+void	fill_str(char *res, char *s1, char *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
 
 	i = 0;
-	len_tot = ft_strlen(s1) + ft_strlen(s2);
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	final = (char *)malloc(sizeof(char) * (len_tot + 1));
-	if (final == NULL)
-		return (NULL);
-	while (s1[i])
-	{
-		final[i] = s1[i];
-		i++;
-	}
-	len_tot = i;
-	i = 0;
-	while (s2[i])
-	{
-		final[len_tot + i] = s2[i];
-		i++;
-	}
-	final[len_tot + i] = '\0';
-	return (final);
+	j = 0;
+	while (s1[j])
+		res[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
 }
+
 
 size_t	ft_strlen(const char *s)
 {
-	int	a;
+	int	i;
 
-	a = 0;
-	while (s[a] != 0)
-		a++;
-	return (a);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-char	*ft_strchr(const char *s, int c)
+/*char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
@@ -65,7 +87,7 @@ char	*ft_strchr(const char *s, int c)
 	if (s[i] == (char)c)
 		return ((char *) &s[i]);
 	return (NULL);
-}
+}*/
 
 char	*ft_strdup(char *src)
 {
